5a087754fb1abb423fac34cc2c5b11b6ed781f24
Tadas Alvikas <tadas.alvikas@yit.lt>
2018-05-15 17:06:54 +0300

    Fifth commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -67,6 +68,6 @@ app.get('/bad', function(req, res) {
 })
 
 
-app.listen(3000, function() {
-    console.log('Server is up on port 3000');
+app.listen(port, function() {
+    console.log('Server is up on port ', port);
 });
